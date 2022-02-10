0e156775b54ec86bd598ff734f23b66528f1e8cc
Luis Monte <luismonte.l560@gmail.com>
2022-02-10 20:19:55 -0300

    1. Crie uma página inicial de login + tentando conf. Redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
 // Coloque aqui suas actions
+function loginAction(payload) {
+  return {
+    type: 'LOGIN',
+    payload,
+  };
+}
+
+export default loginAction;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,101 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import loginAction from '../actions';
+
+const PASSWORD_LENGTH = 6;
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      isButtonDisabled: true,
+      shouldRedirect: false,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value,
+    }, () => this.handleButtonValidation());
+  }
+
+  handleButtonValidation = () => {
+    const { email, password } = this.state;
+    if (email.includes('@')
+    && email.includes('.com')
+    && password.length >= PASSWORD_LENGTH
+    ) {
+      this.setState({ isButtonDisabled: false });
+    } else {
+      this.setState({ isButtonDisabled: true });
+    }
+  }
+
+  handleButton = (event) => {
+    event.preventDefault();
+    const { email } = this.state;
+    const { submitUserInfo } = this.props;
+    submitUserInfo(email);
+    this.setState({ shouldRedirect: true });
+  }
+
   render() {
-    return <div>Login</div>;
+    const {
+      email,
+      password,
+      isButtonDisabled,
+      shouldRedirect,
+    } = this.state;
+    if (shouldRedirect) return <Redirect to="/carteira" />;
+    return (
+      <div>
+        <form>
+          <label htmlFor="emailInput">
+            Email:
+            <input
+              type="email"
+              name="email"
+              id="emailInput"
+              data-testid="email-input"
+              value={ email }
+              onChange={ this.handleChange }
+            />
+          </label>
+
+          <label htmlFor="passwordInput">
+            Senha:
+            <input
+              type="text"
+              name="password"
+              id="passwordInput"
+              data-testid="password-input"
+              value={ password }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="submit"
+            disabled={ isButtonDisabled }
+            onClick={ this.handleButton }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  submitUserInfo: (userEmail) => dispatch(loginAction(userEmail)),
+});
+
+Login.propTypes = {
+  submitUserInfo: PropTypes.func,
+}.isRequired;
+
+export default connect(null, mapDispatchToProps)(Login);
