da8db3de2055f4daa77b37acffedf165c126971b
Luís Monte <luismonte.l560@gmail.com>
2022-09-12 16:51:25 -0300

    Updates

---
+++ precache-manifest.db96d29ee675acc0082b922183cdeea6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c567e897da8d29bab33448b4c3e59637",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "19d0518c510e6f2a421c",
+    "url": "/trybewallet/static/css/main.3433225f.chunk.css"
+  },
+  {
+    "revision": "bcc9de553d3d0e0382d8",
+    "url": "/trybewallet/static/js/2.efd625fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.efd625fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19d0518c510e6f2a421c",
+    "url": "/trybewallet/static/js/main.e609b7b2.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  },
+  {
+    "revision": "699cfeade638ea4aa06294cdfed8e4bb",
+    "url": "/trybewallet/static/media/MyWalletLogo.699cfead.svg"
+  }
+]);
